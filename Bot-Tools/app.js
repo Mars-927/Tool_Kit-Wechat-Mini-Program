25277156a5913987a535b26355b34d950e9b353a
Master <sureshan@foxmail.com>
2021-11-13 21:52:59 +0800

    Upload

--- Bot-Tools/app.js
+++ Bot-Tools/app.js
@@ -1,10 +1,11 @@
 //app.js
+// 此处填写汇率获取API
 App({
   
   globalData:{
       userInfo: null,
       currencyList: {},
       exchangeLastUpdated:"",
-      Api: "https://data.fixer.io/api/latest?access_key=dbf8f3e9b005c4af81f9caf39cd19814"
+      Api: "***" 
   }
 })
